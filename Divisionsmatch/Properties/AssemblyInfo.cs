1b3ea338e8c02bc840af5d946a485557b27d11ef
AndersKlinting <30745841+AndersKlinting@users.noreply.github.com>
2023-03-13 22:59:41 +0100

    v2023
    - ny AutoUpdater.Net
    - support for reglement 2023, inkl løbere i grupper
    - fix af problem med print til html ved blanktegn i username

--- Divisionsmatch/Properties/AssemblyInfo.cs
+++ Divisionsmatch/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Anders Klinting")]
 [assembly: AssemblyProduct("Divisionsmatchberegning")]
-[assembly: AssemblyCopyright("Copyright ©  2013-2022")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.0.1")]
-[assembly: AssemblyFileVersion("2021.0.1")]
+[assembly: AssemblyVersion("2023.0.0")]
+[assembly: AssemblyFileVersion("2023.0.0")]
